79941daf38548668dfda3e75f3681b7a49d65f19
aob123 <alexobeswick@gmail.com>
2020-06-17 00:45:16 +0200

    Added code to create element and appendChild

---
+++ script.js
@@ -0,0 +1,10 @@
+/****************** Creates a new element    ****************/
+
+let mainContainer = document.querySelector("#mainContainer");
+let mainGrid = document.createElement("div");
+
+mainGrid.classList.add("mainGrid");
+mainContainer.appendChild(mainGrid);
+
+/************************************************************/
+
